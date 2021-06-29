6df28f5b00b73225036e7978a4a36fb751a81d5d
S0ULIS <53568453+S0ULIS@users.noreply.github.com>
2021-06-29 20:46:58 +0200

    Added the possibility to restart the progress
    Added the possibility to restart the progress of the current search

--- search_mathematician.py
+++ search_mathematician.py
@@ -3,12 +3,18 @@ from collections import namedtuple
 import wikipedia
 import csv
 
-keywords = ["Matemático","matemático","Matemática","matemática","teorema","matematico","Matematico", "Teorema"]
+keywords = ["Matemático","matemático","Matemática","matemática","teorema","matematico","Matematico","matematica","Matematica", "Teorema","mathematician","Mathematician"]
 wikipedia.set_lang("es")
 
+# Filenames
+
+fichIn = "vial_Sevilla_Bueno.csv"
+fichOut = "matematicos.csv"
 
 
-Calle = namedtuple("Calle", "calle,municipio,nombre")
+
+verbose = False
+Calle = namedtuple("Calle", "calle,municipio,nombre,x,y")
 
 def get_offset():
     try:
@@ -25,12 +31,13 @@ def set_offset(new_offset=-1):
 
 def lee_calles(fichero,offset=0):
     result = []
-    with open("vialSevillaFiltrado.csv", encoding="Latin1") as f:
-        lector = csv.reader(f)
+    with open(fichero,encoding="Latin1") as f:
+        lector = csv.reader(f,delimiter=";")
+        next(lector)
         for i in range(offset-1):
             next(lector)
         for linea in lector:
-            result.append(Calle(linea[0],linea[1],linea[2]))
+            result.append(Calle(linea[11],linea[8],linea[5],linea[9],linea[10]))
     return result
 
 def is_mathematician(calle,keywords):
@@ -41,21 +48,33 @@ def is_mathematician(calle,keywords):
             if key in pagina.content:
                 result=True
     except wikipedia.exceptions.DisambiguationError as e:
-        print("Mas de un resultado disponible")
-        print(e)
+        print("Más de un resultado disponible")
+        if verbose:
+            print(e)
     except wikipedia.exceptions.PageError:
         print("Página no encontrada")
     return result
 
 def anadir_calle(calle):
-    with open("matematicos.csv","a",encoding="Latin1") as f:
-        f.write("{},{},{}\n".format(calle.calle,calle.municipio,calle.nombre))
+    with open(fichOut,"a", encoding="Latin1") as f:
+        f.write("{},{},{},{},{}\n".format(calle.calle,calle.municipio,calle.nombre,calle.x,calle.y))
+
+def restart_files():
+    with open(fichOut,"w", encoding="Latin1") as f:
+        f.write("patrocinado,por,mapamático,:),:)\n")
+    with open("offset.txt","w") as f:
+        f.write("0")
     
 
 if __name__=="__main__":
-    
+    try:
+        opt = int(input("Introduzca 1 para seguir desde donde lo dejó o 2 para empezar de nuevo: "))
+    except:
+        print("Opción no válida")
+    if(opt==2):
+        restart_files()
     offset = get_offset()    
-    calles = lee_calles("vialSevillaFiltrado.csv",offset)
+    calles = lee_calles(fichIn,offset)
     total = len(calles)
     print("Comenzando por {} de un total de {}".format(offset,total+offset))
     for calle in calles:
